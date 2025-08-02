92218faf032e621536b019d5ac89268e8a7dcd1f
Khadija Zouhair <147002128+khzouhair@users.noreply.github.com>
2025-08-02 18:02:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ nlp = None
 def get_nlp():
     global nlp
     if nlp is None:
-        nlp = spacy.load("ro_core_news_sm")
+        nlp = spacy.load("en_core_web_sm")
+        #nlp = spacy.load("ro_core_news_sm")
     return nlp
 # nlp = spacy.load("en_core_web_sm")
 CATEGORIES = get_categories()
